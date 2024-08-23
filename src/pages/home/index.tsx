b187bd9c7bccfb25d3177658ed125fd4ae8e0830
gustavokra <gustavo.kraemer6@gmail.com>
2024-08-23 16:31:33 -0300

    Primeira parte

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (
+        <>
+            <header className="container py-sm">
+                <main>
+                    <p>
+                        Teste
+                    </p>
+                </main>
+            </header>
+        </>
+    )
+}
