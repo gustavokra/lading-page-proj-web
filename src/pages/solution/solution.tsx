0113172e6be70daa4b2e252b5cee45f0b34f5d29
gustavokra <gustavo.kraemer6@gmail.com>
2024-09-14 17:36:16 -0300

    Header fixed

--- src/pages/solution/solution.tsx
+++ src/pages/solution/solution.tsx
@@ -15,27 +15,27 @@ export default function Solution() {
                 <p>
                     Inovação é com a gente! A datasage já conquistou diversos clientes, seja você mais um deles, veja tudo que pode ganhar com nossos serviços.
                 </p>
-
                 <div className="layout">
                     <div className="card">
-                        <img src={Watch} alt="Medalha" width={64} height={64} />
-                        <h3>Ganho de tempo!</h3>
+                        <img src={Watch} alt="Relógio" width={64} height={64} />
+
+                        <h3>Processos mais ágeis!</h3>
                         <p>
                             Faça perguntas ou até mesmo peça ações para seu próprio chat!
                         </p>
                     </div>
                     <div className="card">
-                        <img src={UserCheck} alt="Medalha" width={64} height={64} />
-                        <h3>Treinamento personalizado por domínio</h3>
+                        <img src={UserCheck} alt="Usuário" width={64} height={64} />
+                        <h3>Treinamento personalizado!</h3>
                         <p>
                             Você tem um sistema ou um conjunto de informações e quer um chat personalizado para seu uso? Sem problemas!
                         </p>
                     </div>
                     <div className="card">
-                        <img src={Link} alt="Medalha" width={64} height={64} />
+                        <img src={Link} alt="Link" width={64} height={64} />
                         <h3>Facilidade de integração!</h3>
                         <p>
-                            Você quer integrar processos? Sem problema! Nossa plataforma pode ser integrada com qualquer outra!
+                            Você quer integrar processos? Sem problema! Nossa plataforma pode ser integrada com qualquer sistema!
                         </p>
                     </div>
                 </div>
